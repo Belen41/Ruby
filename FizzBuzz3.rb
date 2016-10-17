9f137ad8b97cbe553e71e8405cdcefe1ab9727db
Belen41 <belenlinacero@gmail.com>
2016-10-17 11:50:07 +0200

    Add files via upload

---
+++ FizzBuzz3.rb
@@ -0,0 +1,15 @@
+i = 1
+while i <= 100 do 
+	if i[0] = 1
+		puts "Bang"
+	elsif i % 3 == 0 && i % 5 == 0 
+		puts "FizzBuzz" 
+	elsif i % 3 == 0
+		puts "Fizz" 
+	elsif i % 5 == 0
+		puts "Buzz"
+	else
+		puts i
+	end
+	i += 1
+end
